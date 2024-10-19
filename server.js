0bddd229594e11231f626eb6d4730620c452d0dd
zbjfei <zbjfei@qq.com>
2024-10-19 21:35:43 +0300

    change port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app.js');
 
-const port = 3000;
+const port = 9000;
 
 app.listen(port, () => { console.log(`Server has started on port ${port}`); }); // eslint-disable-line no-console
